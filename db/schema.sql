5e11fd695189c631d252534f00bfd57705f574b3
Maria Paterno <mariapaterno7@yahoo.com>
2022-03-16 20:16:37 -0600

    Updated readme

--- db/schema.sql
+++ db/schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE department (
 );
 
 CREATE TABLE e_role (
-  id INT NOT NULL PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL,
   department_id INT,
@@ -19,7 +19,7 @@ CREATE TABLE e_role (
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
